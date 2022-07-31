2af5f25177c1ca07e68ff969a1e1b7ff5ac93fbd
Jack Steiner <steiner.jack01@gmail.com>
2022-07-31 15:49:41 -0500

    finished adding all files and html is all set up

--- server/server.js
+++ server/server.js
@@ -1,13 +1,12 @@
 const express = require('express');
 
 const app = express();
+const PORT = process.env.PORT || 5002;
 
-const port = process.env.PORT || 5002;
+app.use(express.urlencoded());
 
 app.use(express.static('server/public'));
 
-let numberOfRequests = 0;
-
-app.listen(port, () => {
-    console.log('Listening on port', port);
+app.listen(PORT, () => {
+    console.log('Listening on port', PORT);
 });
